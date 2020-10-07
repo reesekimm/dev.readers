7e0e46b21abfc211dcba1ac4ba7e7ec86b53573a
Reese Kim <reesekimm@gmail.com>
2020-10-07 16:50:30 +0900

    [#13] feat: 작성한 리뷰 메인페이지에 노출

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
+import { useSelector } from 'react-redux';
 
+import { RootState } from '@features';
 import { MainTemplate, ReviewList } from '@components';
-import reviews from '../assets/reviews';
 
 function Main(): React.ReactElement {
+  const { mainReviews } = useSelector((state: RootState) => state.review);
   return (
     <>
       <Head>
         <title>dev.readers</title>
       </Head>
-      <MainTemplate reviewList={<ReviewList reviews={reviews} />} />
+      <MainTemplate reviewList={<ReviewList reviews={mainReviews} />} />
     </>
   );
 }
