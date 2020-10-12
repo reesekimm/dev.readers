2570596f8fb9b297db83662902a2eed795d04a74
Reese Kim <reesekimm@gmail.com>
2020-10-12 19:31:08 +0900

    [#19] feat: Express-MySQL 연결
    Close #19

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,18 @@ const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
+const db = require('../models');
 const routes = require('./routes');
 const searchRouter = require('./routers/searchRouter');
 
 dotenv.config();
 
 const app = express();
+
+db.sequelize
+  .sync()
+  .then(() => console.log('DB connected!'))
+  .catch(console.error);
 
 app.use(morgan('dev'));
 app.use(cors({ origin: true, credentials: true }));
