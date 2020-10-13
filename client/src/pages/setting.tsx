15f8bdf85bcf5b75f725544a64852fd6f47dd752
Reese Kim <reesekimm@gmail.com>
2020-10-13 17:45:12 +0900

    [#24] chore: 불필요한 페이지 삭제
    Close #24

--- client/src/pages/setting.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { BaseTemplate } from '../components';
-
-function Setting(): React.ReactElement {
-  return <BaseTemplate>Setting</BaseTemplate>;
-}
-
-export default Setting;
